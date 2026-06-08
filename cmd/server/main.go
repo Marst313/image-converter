cef40e10661e431525bd75d84e48a3eb4b6fd089
I Nyoman Dharma <dlegoinyoman1@gmail.com>
2026-06-08 14:15:37 +0700

    Setup for prod

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -24,12 +24,12 @@ func main() {
 	http2.ConfigureServer(server, &http2.Server{})
 	fmt.Println("Server is running on port:", port)
 
-	cert := os.Getenv("CERT_PEM")
-	key := os.Getenv("KEY_PEM")
+	// cert := os.Getenv("CERT_PEM")
+	// key := os.Getenv("KEY_PEM")
 
-	if cert == "" || key == "" {
-		log.Fatal("CERT_PEM and KEY_PEM must be set")
-	}
+	// if cert == "" || key == "" {
+	// 	log.Fatal("CERT_PEM and KEY_PEM must be set")
+	// }
 
 	// ! PROD
 	// err := server.ListenAndServeTLS(cert, key)
@@ -70,7 +70,9 @@ func setupServer() (*http.Server, string) {
 func enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
+		origin := os.Getenv("ORIGIN")
+
+		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
 		w.Header().Set("Access-Control-Expose-Headers", "Content-Disposition")
